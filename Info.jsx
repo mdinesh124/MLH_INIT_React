e92bb07f3d42c2a66897cce377ae5e42da29dee9
mdinesh124 <79132253+mdinesh124@users.noreply.github.com>
2021-06-30 19:46:01 +0530

    Update Info.jsx

--- Info.jsx
+++ Info.jsx
@@ -3,7 +3,7 @@ function Info() {
   return (
     <div className="note">
       <h1>React JS</h1>
-      <p>A web dev bootcamp on Javascript and React JS</p>
+      <p>MLH INIT</p>
     </div>
   );
 }
